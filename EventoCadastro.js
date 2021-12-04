96f5aa77e89b12f02685e780a94bb91f06bc9282
daniel <daniel.jch1896@gmail.com>
2021-12-04 11:33:29 -0300

    Codigo para cadastro de evento no dia 24/12/2021

---
+++ EventoCadastro.js
@@ -0,0 +1,25 @@
+//data de cadastro
+let date1=new Date();
+let date2=new Date('2021-12-24');
+
+if(date1 > date2) {
+    console.log("Evento indisponivel")
+}else{
+    console.log("Continuar cadastro")
+}
+
+//idade da pessoa
+let idadeDaPessoa = 30
+if(idadeDaPessoa >= 18) {
+    console.log("É possivel progredir com o cadastro")
+} else {
+    console.log("Idade insuficiente para realizar o cadastro")
+}
+
+//vagas 
+let númeroDeVagas = 80
+if(númeroDeVagas <= 99) {
+    console.log("É possível finalizar cadastro")
+} else {
+    console.log("Não há vagas disponiveis")
+}
